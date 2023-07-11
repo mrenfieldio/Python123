7b3403d4c23fd991f03618116bea2f384e3fdf9f
ashish <ashishkshaji2002@gmail.com>
2023-07-11 17:54:25 +0530

    ash

---
+++ ashish/ash.py
@@ -0,0 +1,29 @@
+"""a=int(input("enter a number "))
+b=int(input("enter secend number "))
+c=int(input("enter third number "))
+sum=a+b+c
+print("sum of the numbers is",sum)
+avg=sum/3
+print("average is",avg) """
+
+"""a=int(input("enter your birth year "))
+b=int(input("enter your birth month in number "))
+crnt_year=2023
+crnt_month=7
+result1=2023-a
+result2=7-b
+print("your current age is",result1,"year and",result2,"month")"""
+
+
+a=30
+print(type(a))
+print(float(a))
+b=5.86
+print(type(b))
+print(int(b))
+c='6'
+print(type(c))
+print(int(c))
+d="1234"
+print(type(d))
+print(int(d))
