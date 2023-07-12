562f07b3d0a1b614c8c6e339d4729bde1ef8bc85
ashish <ashishkshaji2002@gmail.com>
2023-07-12 17:49:54 +0530

    ash

--- ashish/ash.py
+++ ashish/ash.py
@@ -1,3 +1,4 @@
+
 """a=int(input("enter a number "))
 b=int(input("enter secend number "))
 c=int(input("enter third number "))
@@ -15,7 +16,7 @@ result2=7-b
 print("your current age is",result1,"year and",result2,"month")"""
 
 
-a=30
+'''a=30
 print(type(a))
 print(float(a))
 b=5.86
@@ -26,4 +27,65 @@ print(type(c))
 print(int(c))
 d="1234"
 print(type(d))
-print(int(d))
+print(int(d))'''
+
+
+a=10
+c=50
+
+"""print("the number is",a,"and",b)
+print("sum is ",a+b)
+print("Difference is ",a-b)
+print("Product is ",a*b)
+print("division is ",a/b)
+print("Modulus is ",a%b)
+print("floor division is ",a//b)
+print("exponent is ",a**b)"""
+
+
+#print("the number is",a,"and",c)
+'''a+=5
+print("increment",a)
+a-=5
+print("decrement",a)
+a*=5
+print("*=",a)
+a/=5
+print("/=",a)
+a%=5
+print("%=",a)'''
+
+
+"""print(a==10 and a>=5)
+
+print(a==10 and a<5)
+
+print(a!=10 or a>5)
+
+print(not(a==10))"""
+
+
+b="python"
+"""print('t' in b)
+print('s' in b)
+
+
+print(a>=c)
+print(a==10)
+print(a==c)
+print(a!=c)"""
+
+d="Python"
+print(b is d)
+
+
+
+
+
+
+
+
+
+
+
+
