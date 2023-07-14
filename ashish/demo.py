d15c821e9318645224d618c594be7eef25b9b2ad
ashish <ashishkshaji2002@gmail.com>
2023-07-14 12:21:01 -0700

    ash1

---
+++ ashish/demo.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number "))
+if a>0 :
+    print("positive")
+elif a==0 :
+        print("zero")
+else :
+    print("negetive")
+
+"""a=int(input("enter a number "))
+if a%2==0 :
+    print(a,"is even")
+else :
+     print(a,"is odd")"""
+    
