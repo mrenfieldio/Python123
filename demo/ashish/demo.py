4ceff34e775cd29e2bab4d859f9dc356fe3bfd72
ashish <ashishkshaji2002@gmail.com>
2023-07-18 05:29:14 -0700

    omdt

--- demo/ashish/demo.py
+++ demo/ashish/demo.py
@@ -1,4 +1,4 @@
-a=int(input("enter a number "))
+"""a=int(input("enter a number "))
 if a>0 :
     print("positive")
 elif a==0 :
---
+++ demo/part7.py
@@ -0,0 +1,41 @@
+'''age1=int(input("enter your age "))
+if age1 >=18 :
+    print("you can vote")
+else :
+    print("you are not eligible for voting")'''
+
+'''a1=int(input("enter the first number "))
+a2=int(input("enter the second number "))
+a3=int(input("enter the third number "))
+if a1>a3 and a1>a2 :
+    print(a1,"is greater")
+elif a2>a3 and a2>a1:
+    print(a2,"is greater")
+else:
+    print(a3,"is graeter")'''
+
+
+"""m1=10
+m2=12
+
+print(m1 ,"and",m2)"""
+
+mark1=int(input("enter the mark of maths "))
+mark2=int(input("enter the mark of physics "))
+mark3=int(input("enter the mark of Chemistry "))
+total=mark1+mark2+mark3
+tot2=mark1+mark2
+if mark1>=65  and mark2>=66 and mark3>=50 :
+    if total>=190 and tot2>=140 :
+        print("you are eligible")
+else:
+    print("you are not eligible")
+
+
+
+    
+
+
+
+
+
