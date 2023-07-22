e4555d8fcf4dac9e9e5be6495b130a3590e54869
ashish <ashishkshaji2002@gmail.com>
2023-07-22 05:21:44 -0700

    sad

---
+++ demo/loop5.py
@@ -0,0 +1,22 @@
+#TO CAPITALISE THE FIR ST LETTER
+'''a1=input("enter your full name ")
+a2=a1.lower()
+g=a2.split()
+s1=""
+
+for i in g:
+    if i.islower():
+        j=i[0].capitalize()
+        add = j+i[1:]
+        print(add)
+        s1+=add + ' '
+print(s1)'''
+
+    #REMOVE THE SPECIAL CHARACTER
+
+a1=input("enter some values ")
+print(a2)
+
+    
+
+
